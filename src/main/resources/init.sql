4a31e14a502458cfafe57298ebc83a5be8a06478
Alex <alex.lexx@gmail.com>
2018-06-25 21:39:53 +0200

    parser implementation LOG_TABLE_NAME changed into constant start date parsed into startDateTime

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,2 +1,15 @@
 
 -- todo
+
+--
+-- Table structure for table `loglines`
+--
+
+CREATE TABLE `loglines` (
+  `requestTime` varchar(13) default NULL,
+  `requestIp` varchar(15) default NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+--
+--
